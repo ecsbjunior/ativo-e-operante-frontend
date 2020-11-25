7a17acb4025581a24da495a0400afa7a23ed045d
haaash <ecsb.junior@gmail.com>
2020-11-25 00:09:08 -0300

    api url to backend changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://192.168.0.104:8080"//"https://ativo-e-oprante-backend.herokuapp.com"
+    baseURL: "https://ativo-e-oprante-backend.herokuapp.com" 
 });
 
 export default api;
